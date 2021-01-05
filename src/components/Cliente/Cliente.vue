2dbc2dbee1e33e2567fb6c764be80f6955d1773d
unknown <jonatas-flash@hotmail.com>
2021-01-05 20:15:00 -0300

    start combobox implementation

--- src/components/Cliente/Cliente.vue
+++ src/components/Cliente/Cliente.vue
@@ -4,40 +4,21 @@
     <!-- <InputFild classInput="-id" name="id" label="Id" mask="noString" inputmode="numeric" v-model="clienteId" /> -->
     <p class="id">ID: {{clienteId}}</p>
 
-    <InputFild classInput="-nome" name="nome" label="Nome Cliente" v-model="clienteNome" />
+    <!-- <InputFild classInput="-nome" name="nome" label="Nome Cliente" v-model="clienteNome" /> -->
+
+    <ComboBox v-model="clienteNome"></ComboBox>
 
     <input type="tel" placeholder="celular">
 
-    <select class="combo">
-      <option value="Select">Select....</option>
-      <option value="html">HTML 5</option>
-      <option value="css">CSS 3</option>
-      <option value="javascript">Javascript/Ajax</option>
-      <option value="bootstrap">Bootstrap</option>
-      <option value="react">React JS</option>
-      <option value="vue">Vue JS</option>
-      <option value="2">Angular 2</option>
-      <option value="4">Angular 4</option>
-      <option value="5">Angular 5</option>
-      <option value="6">Angular 6</option>
-      <option value="7">Angular 7</option>
-      <option value="8">Angular 8</option>
-      <option value="CSS fw">CSS framework</option>
-      <option value="rw">Responsive Web Design</option>
-      <option value="dom">DOM scripting</option>
-      <option value="preprocessors">Saas, Less</option>
-      <option value="api">HTML 5 API</option>
-    </select>
+    
    
     <fieldset class="endereco">
       <legend>Endereço</legend>
-
       <InputFild classInput="-rua" name="rua" label="Rua" v-model="clienteRua" />
       <InputFild classInput="-num" name="num" label="Nº" mask="noString" inputmode="numeric" v-model="clienteNum" />
       <InputFild classInput="-bairro" name="bairro" label="Bairro" v-model="clienteBairro" />
       <InputFild classInput="-cidade" name="cidade" label="Cidade" v-model="clienteCidade" />
-      <InputFild classInput="-valor" name="valor" label="Valor Entrega" mask='money' inputmode="numeric" v-model="clienteValor" />   
-      
+      <InputFild classInput="-valor" name="valor" label="Valor Entrega" mask='money' inputmode="numeric" v-model="clienteValor" />     
     </fieldset>
 
     <FlatButton classButton="-save" :handleclick="saveBD" title="Gravar" />
@@ -52,11 +33,13 @@
 <script>
 import InputFild from '@/objects/InputFild/InputFild.vue'
 import FlatButton from '@/objects/FlatButton/FlatButton'
+import ComboBox from '@/objects/ComboBox/ComboBox'
+
 import mask from '@/assets/mask/mask'
 import data from '@/database/data'
 
 export default {
-  components: { InputFild, FlatButton },
+  components: { InputFild, FlatButton, ComboBox },
   
   data() {
     return {
---
+++ src/objects/ComboBox/ComboBox.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="combo">
+  <InputFild classInput="-nome" name="nome" label="Nome Cliente" v-model="localValue" />
+
+  <select class="list" v-model="localValue">
+    
+      <option value="Select">Select....</option>
+      <option value="html">HTML 5</option>
+      <option value="css">CSS 3</option>
+      <option value="javascript">Javascript/Ajax</option>
+      <option value="bootstrap">Bootstrap</option>
+      <option value="react">React JS</option>
+      <option value="vue">Vue JS</option>
+      <option value="2">Angular 2</option>
+      <option value="4">Angular 4</option>
+      <option value="5">Angular 5</option>
+      <option value="6">Angular 6</option>
+      <option value="7">Angular 7</option>
+      <option value="8">Angular 8</option>
+      <option value="CSS fw">CSS framework</option>
+      <option value="rw">Responsive Web Design</option>
+      <option value="dom">DOM scripting</option>
+      <option value="preprocessors">Saas, Less</option>
+      <option value="api">HTML 5 API</option>
+    </select>
+</div>
+</template>
+
+<script>
+import InputFild from '@/objects/InputFild/InputFild.vue'
+export default {
+  components: { InputFild },
+
+  props: {
+    value: { type: String, required: true },
+  },
+  data() {
+    return {
+      localValue: '',
+    }
+  },
+  watch: {
+    value() {
+      this.localValue = this.value
+    },
+    
+  }
+
+}
+</script>
+
+<style src="./style.scss" lang="scss"/>
