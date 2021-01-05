ff7a4bf7c1af53b3cf5810223db8d758e199ab87
unknown <jonatas-flash@hotmail.com>
2021-01-05 19:31:55 -0300

    refactoring group address

--- src/components/Cliente/Cliente.vue
+++ src/components/Cliente/Cliente.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="backgroundDiv customerRegistration">
+  <form class="backgroundDiv customerRegistration">
 
     <!-- <InputFild classInput="-id" name="id" label="Id" mask="noString" inputmode="numeric" v-model="clienteId" /> -->
     <p class="id">ID: {{clienteId}}</p>
 
     <InputFild classInput="-nome" name="nome" label="Nome Cliente" v-model="clienteNome" />
 
-    <div class="endereco">
-      <p>Endereço</p>
+    <input type="tel" placeholder="celular">
+
+    <select class="combo">
+      <option value="Select">Select....</option>
+      <option value="html">HTML 5</option>
+      <option value="css">CSS 3</option>
+      <option value="javascript">Javascript/Ajax</option>
+      <option value="bootstrap">Bootstrap</option>
+      <option value="react">React JS</option>
+      <option value="vue">Vue JS</option>
+      <option value="2">Angular 2</option>
+      <option value="4">Angular 4</option>
+      <option value="5">Angular 5</option>
+      <option value="6">Angular 6</option>
+      <option value="7">Angular 7</option>
+      <option value="8">Angular 8</option>
+      <option value="CSS fw">CSS framework</option>
+      <option value="rw">Responsive Web Design</option>
+      <option value="dom">DOM scripting</option>
+      <option value="preprocessors">Saas, Less</option>
+      <option value="api">HTML 5 API</option>
+    </select>
+   
+    <fieldset class="endereco">
+      <legend>Endereço</legend>
 
       <InputFild classInput="-rua" name="rua" label="Rua" v-model="clienteRua" />
       <InputFild classInput="-num" name="num" label="Nº" mask="noString" inputmode="numeric" v-model="clienteNum" />
@@ -15,14 +38,15 @@
       <InputFild classInput="-cidade" name="cidade" label="Cidade" v-model="clienteCidade" />
       <InputFild classInput="-valor" name="valor" label="Valor Entrega" mask='money' inputmode="numeric" v-model="clienteValor" />   
       
-    </div>
+    </fieldset>
 
     <FlatButton classButton="-save" :handleclick="saveBD" title="Gravar" />
     <FlatButton classButton="-search" :handleclick="searchBD" title="Buscar" />
     <FlatButton classButton="-clean" :handleclick="cleanFilds" title="Limpar" />
+    <input type="reset">
 
 
-  </div>
+  </form>
 </template>
 
 <script>
